2cb1934bcbc7446f5973cfd2d852d86176dbdc27
飯塚歩 <ayumu@iidzukaminoiMac.elecom>
2020-06-21 01:05:10 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {
-        console.log("ikeruyan");
-      }}/>
-    </React.Fragment>
-  )
-}
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {
+//         console.log("ikeruyan");
+//       }}/>
+//     </React.Fragment>
+//   )
+// }
 
+const App = () => {
+  return <div>Hi!!</div>
+}
 export default App;
